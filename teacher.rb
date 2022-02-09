6e6ed3e4794e16fe8fecee09cc93e974b4210c38
rolandm99 <manfulmweze99@gmail.com>
2022-02-09 14:38:02 +0100

    Fix linters errors

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, name, parent_permission)
-    super(age, name, parent_permission)
+  def initialize(specialization, age, _name = 'Unknown', _parent_permission: true)
+    super(specialization, age, _name = 'Unknown', _parent_permission: true)
     @specialization = specialization
   end
 
