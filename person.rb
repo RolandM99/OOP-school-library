c6696f4741a40e299e26623d32b371cda4648d95
rolandm99 <manfulmweze99@gmail.com>
2022-02-10 11:05:52 +0100

    Update person and student files from the UML

--- person.rb
+++ person.rb
@@ -1,15 +1,16 @@
 require './corrector'
-
+require './rental'
 class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, _name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
   def can_use_services?
@@ -17,7 +18,11 @@ class Person
   end
 
   def validate_name
-    @name = @corrector.correct_name(@name)
+    @name = @corrector.correct_name(name)
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 
   private
